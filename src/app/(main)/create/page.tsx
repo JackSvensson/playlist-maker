d71b61b575bc6e21363b245b9828829411993a06
JackSvensson <Jackens1997@gmail.com>
2025-11-14 15:04:41 +0100

    Added header to create

--- src/app/(main)/create/page.tsx
+++ src/app/(main)/create/page.tsx
@@ -1,6 +1,7 @@
-import { auth } from "@/auth"
+import { auth, signOut } from "@/auth"
 import { redirect } from "next/navigation"
 import TrackSearch from "@/components/spotify/TrackSearch"
+import Header from "@/components/Header"
 
 export default async function CreatePage() {
   const session = await auth()
@@ -11,7 +12,14 @@ export default async function CreatePage() {
 
   return (
     <div className="min-h-screen bg-black text-white">
-      <div className="max-w-6xl mx-auto p-8">
+      <Header 
+        user={session.user} 
+        onLogout={async () => {
+          "use server"
+          await signOut({ redirectTo: "/login" })
+        }}
+      />
+      <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
         <TrackSearch />
       </div>
     </div>
